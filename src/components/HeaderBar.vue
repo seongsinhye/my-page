3086bbf13aab01072d2d73e7ad175743bb58d7c3
seongsinhye <sinhye2428@naver.com>
2024-01-28 20:44:57 +0900

    header 컴포넌트 및 init 커밋

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <button type="button" class="btn btn-primary">Primary</button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import router from './routes/index.js';
+
+const app = createApp(App);
+app.use(router);
 
 createApp(App).mount('#app')
